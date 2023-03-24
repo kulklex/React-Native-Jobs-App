1df9c6a2d4a6356f753fbf0143970a33707a34d3
kulklex <kulklex@gmail.com>
2023-03-24 12:59:52 +0100

    Another useFetch() to avoid 429 error

--- utils/useFetchB.js
+++ utils/useFetchB.js
@@ -1,10 +1,13 @@
+    // Note that another useFetch function was created because the external api limits requests due to the fact that we are using a free service
+    // Hence to avoid 429 error, i created a new function that calls only after 10s (a time by which the first api call is expected to have been concluded with)
+
 import React, {useState, useEffect} from "react"
 import axios from "axios"
 import {RAPID_API_KEY} from '@env'
 
 export const rapidAPIKey = RAPID_API_KEY
 
- const useFetch = (endpoint, query) => {
+ const useFetchB = (endpoint, query) => {
     const [data, setData] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
@@ -22,17 +25,19 @@ export const rapidAPIKey = RAPID_API_KEY
     const fetchData = async () => {
         setIsLoading(true)
 
-        try {
-            const {data: {data}} = await axios.request(options)
-
-            setData(data)
-            setIsLoading(false)
-        } catch (error) {
-            setError(error)
-            console.error(error)
-        } finally {
-            setIsLoading(false)
-        }
+        setTimeout(async () => {
+            try {
+                const {data: {data}} = await axios.request(options)
+    
+                setData(data)
+                setIsLoading(false)
+            } catch (error) {
+                setError(error)
+                console.error(error)
+            } finally {
+                setIsLoading(false)
+            }
+        }, 10000)
       }
 
     useEffect(() => {
@@ -52,5 +57,5 @@ export const rapidAPIKey = RAPID_API_KEY
 }
 
 
-export default useFetch
+export default useFetchB
 
