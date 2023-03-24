c1d6c54a2062bc76b3deb3fbc330efd6e6543ede
kulklex <kulklex@gmail.com>
2023-03-24 10:42:51 +0100

    Impl Home page view

--- app/_layout.js
+++ app/_layout.js
@@ -1,8 +1,32 @@
 import { Stack } from "expo-router";
+import { useCallback } from "react";
+import { useFonts } from "expo-font";
+import * as SplashScreen from 'expo-splash-screen'
 
+
+SplashScreen.preventAutoHideAsync()
 
 const Layout = () => {
-    return (<Stack />)
+    const [fontsLoaded] = useFonts({
+        DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+        DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+        DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+      })
+    
+    
+    const onLayoutRootView = useCallback(async () => {
+        // -> Only show the home page if fonts have been loaded  
+        if (fontsLoaded) {
+            await SplashScreen.hideAsync()
+        }
+    }, [fontsLoaded])  
+
+    
+
+    if (!fontsLoaded) return null
+
+
+    return (<Stack onLayout={onLayoutRootView} />)
 }
 
 
